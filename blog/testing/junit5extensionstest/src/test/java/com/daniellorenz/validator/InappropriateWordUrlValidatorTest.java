c5a950faf354c5444a6c08c3098ce2e800f55e3a
Daniel Lorenz <Lorenz-Daniel@t-online.de>
2020-09-15 22:04:28 +0200

    parameterized tests for good and bad urls

--- blog/testing/junit5extensionstest/src/test/java/com/daniellorenz/validator/InappropriateWordUrlValidatorTest.java
+++ blog/testing/junit5extensionstest/src/test/java/com/daniellorenz/validator/InappropriateWordUrlValidatorTest.java
@@ -2,9 +2,12 @@ package com.daniellorenz.validator;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
 
 @ExtendWith(RandomBadUrlResolver.class)
 class InappropriateWordUrlValidatorTest {
@@ -34,35 +37,106 @@ class InappropriateWordUrlValidatorTest {
         boolean result = cut.check(url);
         Assertions.assertFalse(result, "Inappropriate URL should fail.");
     }
+    @Nested
+    class RepeatedTests {
+        @RepeatedTest(5)
+        void inappropriateUrlShouldFail_iterator() {
+            String url = badUrls.next();
+            System.out.println(url);
+            boolean result = cut.check(url);
+            Assertions.assertFalse(result, "Inappropriate URL should fail.");
+        }
 
-    @RepeatedTest(5)
-    void inappropriateUrlShouldFail_iterator(){
-        String url = badUrls.next();
-        System.out.println(url);
-        boolean result = cut.check(url);
-        Assertions.assertFalse(result, "Inappropriate URL should fail.");
+        @RepeatedTest(2)
+        void staticInappropriateUrlShouldFail_iterator() {
+            String url = staticBadUrls.next();
+            System.out.println(url);
+            boolean result = cut.check(url);
+            Assertions.assertFalse(result, "Inappropriate URL should fail.");
+        }
+
+        @RepeatedTest(10)
+        void randomInappropriateUrlShouldFail_local() {
+            String url = badUrlsCollection.get(ThreadLocalRandom.current().nextInt(0, badUrlsCollection.size()));
+            System.out.println(url);
+            boolean result = cut.check(url);
+            Assertions.assertFalse(result, "Inappropriate URL should fail.");
+        }
+
+        @RepeatedTest(10)
+        void randomInappropriateUrlShouldFail(@RandomBadUrl String url) {
+            boolean result = cut.check(url);
+            Assertions.assertFalse(result, "Inappropriate URL(" + url + ") should fail.");
+        }
     }
 
-    @RepeatedTest(2)
-    void staticInappropriateUrlShouldFail_iterator(){
-        String url = staticBadUrls.next();
-        System.out.println(url);
-        boolean result = cut.check(url);
-        Assertions.assertFalse(result, "Inappropriate URL should fail.");
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class ParameterizedTests{
+
+        @ParameterizedTest
+        @ValueSource(strings = { "www.shit.com", "www.daniellorenz.com/shit"})
+        void valueSourceInappropriateUrlShouldFail(String url){
+            boolean result = cut.check(url);
+            Assertions.assertFalse(result, "Inappropriate URL(" + url + ") should fail.");
+        }
+
+        /**
+         * This is difficult in nested test classes.
+         * Typically, the method (allBadUrls) has to be static. However, nested classes cannot have static members.
+         * Moving the static method to the parent class works, but we have to use the fully qualified name.
+         * A possible workaround is the @TestInstance(TestInstace.Lifecycle.PER_CLASS) annotation, which allows the
+         * method source to be non-static, however now all tests of this nested class are executed using the same
+         * class instance.
+         *
+         */
+        @ParameterizedTest
+        @MethodSource("allBadUrls")
+        void methodSourceInappropriateUrlShouldFail(String url){
+            boolean result = cut.check(url);
+            Assertions.assertFalse(result, "Inappropriate URL(" + url + ") should fail.");
+        }
+
+        private  Stream<String> allBadUrls(){
+            return badUrlsCollection.stream();
+        }
+
+        @ParameterizedTest
+        @MethodSource("com.daniellorenz.validator.InappropriateWordUrlValidatorTest#allBadUrls")
+        void staticMethodSourceInappropriateUrlShouldFail(String url){
+            boolean result = cut.check(url);
+            Assertions.assertFalse(result, "Inappropriate URL(" + url + ") should fail.");
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "www.shit.com",
+                "www.daniellorenz.com/shit"
+        })
+        void csvSourceInappropriateUrlShouldFail(String url) {
+            boolean result = cut.check(url);
+            Assertions.assertFalse(result, "Inappropriate URL(" + url + ") should fail.");
+        }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = {"/allValidUrls.csv"})
+        void allValidUrlsShouldPass(String url){
+            boolean result = cut.check(url);
+            Assertions.assertTrue(result, "Valid URL should pass.");
+        }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = {"/allBadUrls.csv"})
+        void allInvalidUrlsShouldFail(String url){
+            boolean result = cut.check(url);
+            Assertions.assertFalse(result, "Inappropriate URL should fail.");
+        }
+
     }
 
-    @RepeatedTest(10)
-    void randomInappropriateUrlShouldFail_local(){
-        String url = badUrlsCollection.get(ThreadLocalRandom.current().nextInt(0, badUrlsCollection.size()));
-        System.out.println(url);
-        boolean result = cut.check(url);
-        Assertions.assertFalse(result, "Inappropriate URL should fail.");
+    private static Stream<String> allBadUrls(){
+        return badUrlsCollection.stream();
     }
 
-    @RepeatedTest(10)
-    void randomInappropriateUrlShouldFail(@RandomBadUrl String url){
-        boolean result = cut.check(url);
-        Assertions.assertFalse(result, "Inappropriate URL("+url+") should fail.");
-    }
 
 }
