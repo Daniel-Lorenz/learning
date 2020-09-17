d38e27dc24a7ab19171c265baad946f4dce1f4bc
Daniel Lorenz <Lorenz-Daniel@t-online.de>
2020-09-17 22:20:56 +0200

    cds init with no changes to cds-maven-plugin

---
+++ blog/cds-series/cdsplugin/srv/src/main/java/com/handlers/CatalogServiceHandler.java
@@ -0,0 +1,26 @@
+package com.handlers;
+
+import java.util.stream.Stream;
+
+import org.springframework.stereotype.Component;
+
+import com.sap.cds.services.cds.CdsService;
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.After;
+import com.sap.cds.services.handler.annotations.ServiceName;
+
+import catalogservice.CatalogService_;
+import catalogservice.Books;
+
+@Component
+@ServiceName(CatalogService_.CDS_NAME)
+public class CatalogServiceHandler implements EventHandler {
+
+	@After(event = CdsService.EVENT_READ)
+	public void discountBooks(Stream<Books> books) {
+		books.filter(b -> b.getTitle() != null && b.getStock() != null)
+		.filter(b -> b.getStock() > 200)
+		.forEach(b -> b.setTitle(b.getTitle() + " (discounted)"));
+	}
+
+}
