4d5d73e882b3ce08a10ffc8630f64519805f007e
Daniel Lorenz <Lorenz-Daniel@t-online.de>
2020-10-02 22:53:46 +0200

    service endpoint tests and stream tests

--- blog/cds-series/cds-struct-blog/srv/src/main/java/com/daniellorenz/blog/bookshop/BookshopHandler.java
+++ blog/cds-series/cds-struct-blog/srv/src/main/java/com/daniellorenz/blog/bookshop/BookshopHandler.java
@@ -2,25 +2,36 @@ package com.daniellorenz.blog.bookshop;
 
 import bookservice.BookService_;
 import bookservice.Books_;
+import bookshop.Books;
 import carshop.Cars;
 import com.sap.cds.services.cds.CdsReadEventContext;
 import com.sap.cds.services.cds.CdsService;
-import com.sap.cds.services.handler.annotations.After;
-import com.sap.cds.services.handler.annotations.Before;
+import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.*;
 
+@Component
 @ServiceName(BookService_.CDS_NAME)
-public class BookshopHandler {
+public class BookshopHandler implements EventHandler {
 
 
     @On(event = CdsService.EVENT_READ, entity = Books_.CDS_NAME)
-    public void proposeMapAsResult(CdsReadEventContext context){
+    public void proposeMapAsResult(CdsReadEventContext context) {
         Map<String, Object> dummy = new HashMap<>();
         dummy.put(Cars.COLOR, "red");
+        dummy.put(Books.AUTHOR, "Daniel");
+        dummy.put(Books.TITLE, "Title");
+        dummy.put(Books.ID, UUID.randomUUID().toString());
 
-        context.setResult(Collections.singletonList(dummy));
+        Map<String, Object> dummy2 = new HashMap<>();
+        dummy2.put(Books.TITLE, "Title 2");
+        dummy2.put(Books.ID, UUID.randomUUID().toString());
+
+        context.setResult(List.of(dummy, dummy2));
+        context.setCompleted();
     }
 }
--- blog/cds-series/cds-struct-blog/srv/src/test/java/com/daniellorenz/blog/cds/CdsStructTest.java
+++ blog/cds-series/cds-struct-blog/srv/src/test/java/com/daniellorenz/blog/cds/CdsStructTest.java
@@ -13,98 +13,126 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 public class CdsStructTest {
 
-    @Test
-    void createBookWithStruct() {
+    @Nested
+    class StructAccessTests {
+        @Test
+        void createBookWithStruct() {
 
-        bookshop.Books book = Struct.create(bookshop.Books.class);
-        Assertions.assertNotNull(book);
-        Assertions.assertTrue(book.isEmpty());
+            bookshop.Books book = Struct.create(bookshop.Books.class);
+            Assertions.assertNotNull(book);
+            Assertions.assertTrue(book.isEmpty());
+        }
+
+        @Test
+        void createBookWithStaticMethod() {
+            bookshop.Books book = bookshop.Books.create();
+            Assertions.assertNotNull(book);
+            Assertions.assertTrue(book.isEmpty());
+        }
+
+        @Test
+        void convertMapToBook() {
+            Map<String, Object> bookMap = new HashMap<>();
+            bookMap.put(Books.AUTHOR, "Daniel Lorenz");
+            bookMap.put(Books.TITLE, "A short book about nothing");
+            Books book = Struct.access(bookMap).as(Books.class);
+
+            Assertions.assertEquals(bookMap.get(Books.TITLE), book.getTitle());
+            Assertions.assertEquals(bookMap.get(Books.AUTHOR), book.getAuthor());
+        }
+
+        @Test
+        void accessIsMutable() {
+            Map<String, Object> bookMap = new HashMap<>();
+            bookMap.put(Books.AUTHOR, "Daniel Lorenz");
+            bookMap.put(Books.TITLE, "A short book about nothing");
+            Books book = Struct.access(bookMap).as(Books.class);
+            bookMap.put(Books.AUTHOR, "Some other guy");
+
+            Assertions.assertEquals(bookMap.get(Books.TITLE), book.getTitle());
+            Assertions.assertEquals(bookMap.get(Books.AUTHOR), book.getAuthor());
+        }
+
+        @Test
+        void accessAsView() {
+            Books book = Books.create();
+            book.setAuthor("Daniel Lorenz");
+            book.setTitle("Some Title");
+            book.setId(UUID.randomUUID().toString());
+
+            Titles titleView = Struct.access(book).as(Titles.class);
+
+            Assertions.assertEquals(book.getId(), titleView.getId());
+            Assertions.assertEquals(book.getTitle(), titleView.getTitle());
+            Assertions.assertEquals(book.getAuthor(), titleView.get(Books.AUTHOR));
+        }
+
+        @Test
+        void accessAsDifferentEntity() {
+            Books book = Books.create();
+            book.setAuthor("Daniel Lorenz");
+            book.setTitle("Some Title");
+            book.setId(UUID.randomUUID().toString());
+
+            Cars car = Struct.access(book).as(Cars.class);
+
+            Assertions.assertEquals(book.getId(), car.getId());
+            Assertions.assertEquals(book.getTitle(), car.get(Books.TITLE));
+            Assertions.assertEquals(book.getAuthor(), car.get(Books.AUTHOR));
+
+            Assertions.assertNull(car.getColor());
+            Assertions.assertNull(car.getModel());
+        }
     }
 
-    @Test
-    void createBookWithStaticMethod(){
-        bookshop.Books book = bookshop.Books.create();
-        Assertions.assertNotNull(book);
-        Assertions.assertTrue(book.isEmpty());
+    @Nested
+    class StructStreamTests{
+
+        @Test
+        void testAsStream(){
+
+            List<Books> list = new ArrayList<>();
+            for (int i = 0; i < 100; i++) {
+                list.add(randomBook());
+            }
+
+            List<Cars> result = Struct.stream(list).as(Cars.class).collect(Collectors.toList());
+            list.remove(0);
+            list.get(0).setTitle("Different");
+            Assertions.assertEquals(100, result.size());
+            Assertions.assertEquals(1, result.stream().filter(car -> car.get(Books.TITLE).toString().contentEquals("Different")).count());
+        }
     }
 
-    @Test
-    void convertMapToBook(){
-        Map<String, Object> bookMap = new HashMap<>();
-        bookMap.put(Books.AUTHOR, "Daniel Lorenz");
-        bookMap.put(Books.TITLE, "A short book about nothing");
-        Books book = Struct.access(bookMap).as(Books.class);
-
-        Assertions.assertEquals(bookMap.get(Books.TITLE), book.getTitle());
-        Assertions.assertEquals(bookMap.get(Books.AUTHOR), book.getAuthor());
-    }
-
-    @Test
-    void accessIsMutable(){
-        Map<String, Object> bookMap = new HashMap<>();
-        bookMap.put(Books.AUTHOR, "Daniel Lorenz");
-        bookMap.put(Books.TITLE, "A short book about nothing");
-        Books book = Struct.access(bookMap).as(Books.class);
-        bookMap.put(Books.AUTHOR, "Some other guy");
-
-        Assertions.assertEquals(bookMap.get(Books.TITLE), book.getTitle());
-        Assertions.assertEquals(bookMap.get(Books.AUTHOR), book.getAuthor());
-    }
-
-    @Test
-    void accessAsView(){
+    private Books randomBook() {
         Books book = Books.create();
-        book.setAuthor("Daniel Lorenz");
-        book.setTitle("Some Title");
         book.setId(UUID.randomUUID().toString());
-
-        Titles titleView = Struct.access(book).as(Titles.class);
-
-        Assertions.assertEquals(book.getId(), titleView.getId());
-        Assertions.assertEquals(book.getTitle(), titleView.getTitle());
-        Assertions.assertEquals(book.getAuthor(), titleView.get(Books.AUTHOR));
-    }
-
-    @Test
-    void accessAsDifferentEntity(){
-        Books book = Books.create();
-        book.setAuthor("Daniel Lorenz");
-        book.setTitle("Some Title");
-        book.setId(UUID.randomUUID().toString());
-
-        Cars car = Struct.access(book).as(Cars.class);
-
-        Assertions.assertEquals(book.getId(), car.getId());
-        Assertions.assertEquals(book.getTitle(), car.get(Books.TITLE));
-        Assertions.assertEquals(book.getAuthor(), car.get(Books.AUTHOR));
-
-        Assertions.assertNull(car.getColor());
-        Assertions.assertNull(car.getModel());
+        book.setAuthor("Daniel " + ThreadLocalRandom.current().nextInt());
+        book.setTitle("Some book " + ThreadLocalRandom.current().nextInt());
+        return book;
     }
 
     @Nested
     @SpringBootTest
     @AutoConfigureMockMvc
-    class TestServiceEndpoints{
+    class TestServiceEndpoints {
+        public static final String PATH = "/odata/v4/BookService/Books";
         @Autowired
         private MockMvc mockMvc;
 
         @Test
-        void customHandlerReturnsMap(){
+        void customHandlerReturnsMap() throws Exception {
 
-            mockMvc.perform(MockMvcRequestBuilders.get())
+            String response = mockMvc.perform(MockMvcRequestBuilders.get(PATH)).andReturn().getResponse().getContentAsString();
+            System.out.println(response);
         }
 
-        @Test
-        void customHandlerReturnsMapWithMoreKeys(){
-
-        }
     }
 
 }
